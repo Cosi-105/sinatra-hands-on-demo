2f61ccade7bd2b2edcf6161f558f9617b02a00e2
Pito Salas <pitosalas@gmail.com>
2015-10-30 14:36:22 -0400

    fixing activerecord

--- config/environments.rb
+++ config/environments.rb
@@ -8,17 +8,17 @@ require 'pry-byebug'
 
 configure :production do
   puts "[production environment: ENV=#{ENV['DATABASE_URL']}]"
-#  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-  )
+# #  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+#   ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+#
+#   ActiveRecord::Base.establish_connection(
+#       :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#       :host     => db.host,
+#       :username => db.user,
+#       :password => db.password,
+#       :database => db.path[1..-1],
+#       :encoding => 'utf8'
+#   )
 end
 
 configure :development, :test do
