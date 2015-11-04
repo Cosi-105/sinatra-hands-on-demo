34f22b1ec4fe2b0ba9fae56dc6ab7b3feff9e993
Pito Salas <pitosalas@gmail.com>
2015-11-03 21:01:57 -0500

    keep on tweaking

--- test/event_test.rb
+++ test/event_test.rb
@@ -2,6 +2,7 @@ require_relative './test_helper.rb'
 
 describe Event do
   it "can add events" do
+    puts Event.all.inspect
     Event.create(name: "An Event")
     Event.create(name: "Another Event")
     Event.all.size.must_equal 2
